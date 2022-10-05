43f4a64771a5adf543d1391c983eac4ed98ef970
Moringa School <dwangui799@gmail.com>
2022-10-05 15:16:15 +0300

    complited assighnment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solutions in this file
+// const names = ["Guada lupe", "Ollie," "Aki"];
+function writeCards (names, event) {
+  let newArray = [];
+   for (let i = 0; i < names . length; i ++) {
+    newArray . push ('Thank you, $ {names [i] },for the wonderful ${event} gift !');
+       debugger;
+
+   }
+   return  newArray;
+}
+
+function countDown () {
+    let i =10;
+    while (i > _1) {
+      console.log (i --);
+
+    }
+    return i;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
